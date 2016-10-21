b4f19368335a36bae1975832072f0addb67efd7d
Peter Valdez <peter@nycmesh.net>
2016-10-21 22:45:21 +0700

    Use tar instead of zip, and clean up the tarballs

--- overlay/scripts/backup.sh
+++ overlay/scripts/backup.sh
@@ -3,24 +3,34 @@
 TIMESTAMP=`date +%Y%m%d-%H%M%S`
 BUCKETS=`ls /backups`
 
+echo "Starting scheduled backup. (`date '+%D %T UTC'`)"
+
 for BUCKET in $BUCKETS
 do
   BACKUPS=`ls /backups/$BUCKET`
-  ZIPPED_BUCKET_DIR=/tmp/$BUCKET
+  BUCKET_TARBALL_DIR=/tmp/$BUCKET
 
   # Make a directory for the zipped backups
-  mkdir $ZIPPED_BUCKET_DIR
+  mkdir $BUCKET_TARBALL_DIR
 
-  # Zip up the backups
+  # Make the backup tarballs
   for BACKUP in $BACKUPS
   do
-    ZIPPED_BACKUP_DIR=$ZIPPED_BUCKET_DIR/$BACKUP
+    BACKUP_TARBALL_DIR=$BUCKET_TARBALL_DIR/$BACKUP
+    BACKUP_TARBALL=$BACKUP-$TIMESTAMP.tar.bz2
 
-    # Create the zipped backup directory and zip up the files
-    mkdir $ZIPPED_BACKUP_DIR
-    zip -r9 $ZIPPED_BACKUP_DIR/$BACKUP-$TIMESTAMP.zip /backups/$BUCKET/$BACKUP > /dev/null
+    # Create the temp tarball directory and compress the files
+    mkdir $BACKUP_TARBALL_DIR
+    tar cvjf $BACKUP_TARBALL_DIR/$BACKUP_TARBALL -C /backups/$BUCKET $BACKUP > /dev/null
+    echo "Tarred up '$BACKUP'."
   done
 
   # Upload the backups to S3
-  aws s3 cp --recursive $ZIPPED_BUCKET_DIR s3://$BUCKET/
+  aws s3 cp --recursive $BUCKET_TARBALL_DIR s3://$BUCKET/ > /dev/null
+  echo "Uploaded tarballs to bucket '$BUCKET'."
+
+  # Clean up the zipped files
+  rm -rf $BUCKET_TARBALL_DIR
 done
+
+echo "Backups completed."
